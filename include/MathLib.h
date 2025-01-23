62cea168fb14a341e4e2ee44aacf8453d5672c31
Yuleo <1173012900@qq.com>
2025-01-23 21:52:00 +0800

    mathlib

---
+++ include/MathLib.h
@@ -0,0 +1,14 @@
+#ifndef MATHLIB_H_
+#define MATHLIB_H_
+
+#include <iostream>
+
+namespace MMath{
+    int madd(int a,int b);
+    int mminus(int a,int b);
+    int mmul(int a,int b);
+    int mdiv(int a,int b);
+    void print_test();
+}
+
+#endif
---
+++ src/MathLib.cpp
@@ -0,0 +1,22 @@
+#include "MathLib.h"
+
+namespace MMath{
+    int madd(int a,int b){
+        return a + b;
+    }
+
+    int mminus(int a,int b){
+        return a - b;
+    }
+
+    int mmul(int a,int b){
+        return a * b;
+    }
+
+    int mdiv(int a,int b){
+        return a / b;
+    }
+    void print_test(){
+        std::cout<<"牛犇啊，用上了我的库"<<std::endl;
+    }
+}
